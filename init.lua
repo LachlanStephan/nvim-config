d9ba8e564b3f0e16d99c62abde655de29616b031
Lachlan Stephan <ljstephan116@gmail.com>
2023-02-21 21:29:47 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -60,9 +60,24 @@ require('packer').startup(function(use)
 
   -- set termguicolors to enable highlight groups
   vim.opt.termguicolors = true
+    
+  local function open_nvim_tree(data)
+      
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
 
-  -- empty setup using defaults
-  require("nvim-tree").setup()
+    if not directory then
+      return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+  end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
   -- OR setup with some options
   require("nvim-tree").setup({
